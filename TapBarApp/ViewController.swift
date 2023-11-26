519b8ba458822e5e6f5b97e47cafe0808e1d36dd
alexsokolov <frank9154@gmail.com>
2023-11-27 00:01:23 +0300

    Initial Commit

---
+++ TapBarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TapBarApp
+//
+//  Created by Александр Соколов on 27.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
